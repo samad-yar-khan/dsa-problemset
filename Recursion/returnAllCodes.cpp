cb06baa2500b9c1ef07a42ddb12210d312a08419
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-05-19 17:14:32 +0530

    Do return all codes

--- Recursion/returnAllCodes.cpp
+++ Recursion/returnAllCodes.cpp
@@ -25,34 +25,66 @@ using namespace std;
 
 int getCodes(string input, string output[10000]) {
 
-    //base case
-    if(input.length() == 0){//for empty string ans would be empy string
+    //bc
+    if(input.length() == 0){
+        //ek hi answer hos akte hai nulll
         output[0] = "";
         return 1;
     }
 
-    //ih 
-    //we need to find first num and first two nums
-    int firstDigit = input.at(0) - '0';
-    int secondDigit = 0;
+    //induction hypothesis
 
-    if(input.length()>1){
-        int secondDigit = input.at(1) - '0';    
+    int firstNum = input[0] - '0';
+
+    //so now we need to call rec on smaler string
+    int smallOutput1 = getCodes(input.substr(1) , output);//if 112 is our string , this gives ans for 12 and we append 1s code in this
+    
+    int smallOutput2=0;
+    string output2[10000]; 
+    int secondNum = 0 ;//secon digit 
+    if(input.length() > 1){
+        secondNum = (10*firstNum) + (input[1] - '0');
     }
 
-    int tempNumber = (secondDigit*10) + firstDigit;
-    //we get codes with substring of length n-1
-    int codesOp1 = getCodes(input.substr(1) , output);
+    if(input.length() > 1 && secondNum<27){
 
-    if(tempNumber<=26)
+        smallOutput2= getCodes(input.substr(2) , output2);//2 for 11
 
+    }
+    
+    //induction step
 
+    // now we first need to appedn our first number scode  to the output strig from case1
+    char a='a';
 
+    for(int i = 0 ; i < smallOutput1 ; i++){
+        char ourChar = a+ firstNum-1;
+        output[i] = ourChar + output[i];
+    }
 
-    delete [] output;
+    for(int i = 0 ; i < smallOutput2 ; i++){
+        char ourChar = a+secondNum-1;
+        output[i+smallOutput1] = ourChar+ output2[i];
 
+    }
+
+    return smallOutput1 + smallOutput2;
 }
 
 
+int main(){
+
+    string inp{""};
+    cin>>inp;
+
+    string output[10000];
+
+    int op = getCodes(inp , output);
+
+    for(int i = 0 ; i < op ; i++){
+        cout<<output[i]<<"\n";
+    }
+
+}
 
 
