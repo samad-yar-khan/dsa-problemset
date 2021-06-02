e17e1d963f34b2a96bee440a185002509812558a
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-03 05:29:37 +0530

    Complete - Trees Post order

---
+++ Trees/treesPostOrder.cpp
@@ -0,0 +1,15 @@
+#include "TreeNode.h"
+
+void printPostOrder(TreeNode<int>* root) {
+
+    if(root == NULL){
+        return ;
+    }
+
+    for(int i = 0;i<root->children.size() ; i++){
+        printPostOrder(root->children[i]);
+    }
+
+    cout<<root->data<<" ";
+
+}
