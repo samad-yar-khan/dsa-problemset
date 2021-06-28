dc25c248f41063a62fb2eb6114f708a6085e1499
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-28 20:15:14 +0530

    Complete - FastExponentiation

---
+++ BitManipulation/fastExponentiation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#define ll long long
+#define modVal 1000000007
+using namespace std;
+
+
+ll exp(ll m , ll n){    //m^n
+
+    ll number = m ;
+    ll power = n ;
+    ll answer = 1;
+
+    while( power > 0){
+
+        if((power & 1 ) > 0){
+            answer *= number ;
+            answer%=modVal;
+        } 
+
+        //square the number 
+        number*=number;
+        number%=modVal;
+        //right shift power
+        power = power>>1;
+    }
+
+    return answer;
+
+
+}
+
+int main(){
+
+    ll T{0};
+    cin>>T;
+
+    while(T--){
+        ll m = 0 , n = 0 ;
+        cin>>m>>n;
+        cout<<exp(m,n)<<"\n";
+    }
+   
+
+
+}
---
+++ BitManipulation/fastExponetiation2.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/powx-n/
+
+#include <iostream>
+#define ll long long
+#define modVal 1000000007
+using namespace std;
+
+    double myPow(double x, int n) {
+        
+        // we store these values 
+        double number = x;
+        long long power = n ;
+        
+        //we just store this bool value to keep a track if our power is negative
+        bool isPowerNegative = ( (n<0) ? true : false );
+        if( isPowerNegative){
+            power= (-1)*power;
+        }       
+        //how we solve teh qn is that we keep see our power as bbits in binary format
+        //if m=3 & n = 5 then => n = 101 >> and 3^5 ==> 3^(bin(101)  ==> 3^(4*1 + 2*0  + 1*1) ==>
+        // 3^(4) * 3^(2*0) * 3^(1*1)
+        //so at each index of bits , my power of three is swuare and if the bit is set , then
+        //we will just multiplye that power to our answer
+        double answer = 1 ;
+        
+        while( power > 0){
+                
+            if((power&1) > 0)
+            {
+                answer *=  number;
+            }
+            
+            number*=number;
+            power = power>>1;
+            
+        }
+        
+        //for negative power ans will be 1/ans
+        if(isPowerNegative){
+            answer = 1/answer;
+        }
+        
+        return answer;
+        
+        
+    }
