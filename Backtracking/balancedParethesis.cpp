ed49d712ae2f6e594ce2c7f903fd520fad147ae5
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-07-03 01:47:08 +0530

    Complete - balanced parenthesis backtracking

---
+++ Backtracking/balancedParethesis.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+void helper(int leftBrackets , int rightBrackets , string &s  , vector<string>&vec ,int n , int index ){
+
+    //base case
+    if(leftBrackets==0 && rightBrackets==0 && index == 2*n ){
+        vec.push_back(s);    
+        return;
+    }
+
+    //edge case
+    if(rightBrackets < leftBrackets){
+        return;
+    }
+
+    //ih
+    if(leftBrackets > 0){
+        s.at(index) = '(';
+        helper(leftBrackets -1 , rightBrackets , s , vec , n , index+1);
+    }
+
+    if(rightBrackets > leftBrackets){
+        s.at(index) = ')';
+        helper(leftBrackets  , rightBrackets -1 , s , vec , n , index+1);
+    }
+
+    s.at(index) = '\0';
+
+
+}
+
+vector<string> AllParenthesis(int n) 
+{
+        // Your code goes here 
+        vector<string> ans;
+        string s(2*n +1 , '\0');
+        helper(n , n , s ,ans , n , 0);
+        return ans;
+}
