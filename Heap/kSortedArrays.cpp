a99db926a99d8040e92cccda6c3be73971464d0b
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-07-12 01:57:20 +0530

    Complete - merge ksorted array

--- Heap/kSortedArrays.cpp
+++ Heap/kSortedArrays.cpp
@@ -2,54 +2,104 @@
 #include<iostream>
 using namespace std;
 
-class triplet {
+class triplet{
 
-    public :
-    int array_num ;
-    int my_index ;
+    public:
     int val;
-
-    triplet (int arr_num , int val , int index){
-        this->array_num = arr_num;
+    int index;
+    int row;
+    
+    triplet(int val ,int index ,int row){
         this->val = val;
-        this->my_index = index;
-
+        this->index = index ;
+        this->row = row;
     }
+    triplet(){
+        this->val = 0;
+        this->index = 0 ;
+        this->row = 0;
+    }
+
 };
 
-//comparator class
-class Comp {
+class compare{
+
     public :
 
-    bool operator()(triplet *a,triplet* b) {
+    bool operator()(triplet* a , triplet* b){
         return a->val > b->val;
     }
+
 };
 
+  vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
 
-vector<int> mergeKArrays(vector<vector<int>> arr, int K){
-        //code here
-    vector<int> ans;
-    priority_queue<triplet* , vector<triplet*> , Comp> pq;
-    for(int i = 0 ; i < arr.size() ; i++){
-        triplet* a = new triplet(i , arr[i][0] , 0);
-        pq.push(a);
+        priority_queue<triplet* , vector<triplet*> , compare> pq;
+
+            for(int i  = 0 ; i < arr.size() ; i++){
+
+                triplet* trip = new triplet();
+                trip->val = arr[i][0];
+                trip->row = i;
+                trip->index = 0;
+                pq.push(trip);
+            }   
+        
+        vector<int> sortedVec;
+
+        while(!pq.empty()){
+
+            triplet* ttop = pq.top();
+            pq.pop();
+
+            sortedVec.push_back(ttop->val);
+            int row = ttop->row;
+            int rowSize = arr[row].size();
+            int nextIndex = ttop->index + 1;
+            if(nextIndex < rowSize){
+
+                 triplet* trip = new triplet();
+                trip->val = arr[row][nextIndex];
+                trip->row = row;
+                trip->index = nextIndex;
+
+                pq.push(trip);
+
+            }
+
+
+        }
+
+        return sortedVec;
+
     }
 
-    while(!pq.empty()){
 
-        triplet* t = pq.top();
-        pq.pop();
+int main(){
 
-        ans.push_back(t->val);
-        int row = t->array_num;
-        int index = t->my_index;
-        index++;
-       if(index < arr.at(row).size()){
-           triplet* t2 = new triplet(row , arr[row][index] , index);
-       }
+    vector<vector<int>> vec;
+    int n = 0 , m= 0 ;
+    cin>>n>>m ;
+    for(int i = 0 ; i < n ; i++){
+
+        vector<int> row_i;
+        for(int j = 0 ; j < m ; j++){
+
+            int a = 0 ;
+            cin>>a;
+            row_i.push_back(a);
+
+        }
+
+        vec.push_back(row_i);
     }
 
-    return ans;
+
+    vector<int> ans = mergeKArrays(vec  , m);
+    for(int i = 0 ; i < n*n ; i++ ){
+        cout<<ans[i]<<" ";
+    }
+
 
 }
