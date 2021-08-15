6794324fd9d235073fd820df4fcf45d6ffa4f8ce
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-08-15 20:51:31 +0530

    Complete SPOJ Load balancing

--- Greedy/loadBalancerSPOJ.cpp
+++ Greedy/loadBalancerSPOJ.cpp
@@ -2,4 +2,95 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long o
+#define ll long long 
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n =1;
+    cin>>n;
+    while(n>0){
+        
+        ll*arr =new ll[n];
+        ll*sum = new ll[n];
+
+        ll s = 0  ;
+        for(int i  = 0 ;i < n ; i++){
+            cin>>arr[i];
+            s+=arr[i];
+            sum[i] = s;
+        }
+        ll avg = s/n;
+        if(s%n!=0){
+            cout<<-1<<"\n";
+        }else{
+
+             ll diff = INT_MIN;
+
+            for(int i = 0 ;i < n-1 ; i++){
+
+                ll temp = abs(avg*(i+1)-sum[i]);
+                diff = max(diff , temp);
+
+            }
+            cout<<diff<<"\n";
+        }       
+    
+        cin>>n;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
