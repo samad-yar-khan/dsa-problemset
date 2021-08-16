47eed656232f72fc8fc047b71eada45b9bf9c5ac
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-08-16 23:35:25 +0530

    Incomplete defkin

---
+++ Greedy/Defkin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll T=0;
+    cin>>T;
+    while(T--){
+
+        
+
+    }
+
+}
+
