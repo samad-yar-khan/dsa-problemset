bf124342ab53f0e6dbeff49d7cc27a7f0c0bfae4
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-08-24 18:18:05 +0530

    Complete Sell K stocks DP

--- DP/maxProfitKStocks.cpp
+++ DP/maxProfitKStocks.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 //https://practice.geeksforgeeks.org/problems/maximum-profit4657/1#
 
+
+//memoisation
 int profit(int K , int N , int A[] , int sell , int moneySpent,pair<int , int> dp[502][202][2] ){
         
         if(N==0 || (K==0 && sell==0)){
@@ -59,6 +61,45 @@ int profit(int K , int N , int A[] , int sell , int moneySpent,pair<int , int> d
         }
         
         return profit(K , N , A , 0 , 0 , dp);
+    }
+
+
+    //chnaging the approach to the question
+    //we look for  a dp[k][d] which reprsnets the profit we can  ammak eif we sell k stocks by dth day
+
+ int maxProfit(int K, int N, int A[]) {
+        // code here
+        
+        int**profit = new int *[K+1];
+        for(int i = 0 ; i<=K ; i++){
+            profit[i] = new int[N];
+            for(int j = 0 ; j <N ; j++){
+                profit[i][j] = 0 ;
+            }
+        }
+
+        //first row and col will be zero
+        //FR -> Because you cant make any profit by selling 0 stocks
+        //FC -> Becase even by making a a 100 transacrtion in same dday net profit will be zero
+    
+        for(int k = 1 ; k <= K ; k++){
+            //either ans will be by seling k stocks on day before
+            //what we neeed yp chec is ki aagar mai mujhse pehle wwale sare days mai k-1 bechdu and us din se apna transaction karu to kya max profit hoga
+            //this profit calcucation would have otherwise taken a loop going from 0 to days-1 each time but it is optimised by taking a max value
+            //proit would be = profitBySelling K-1 products from day0 to days[x] + price[days] - days[x] (X:0 -> N)
+            int maxProfit=profit[k-1][0] - A[0];
+            for(int days = 1 ; days < N ; days ++){
+                maxProfit= max(profit[k-1][days-1] - A[days-1], maxProfit);
+                profit[k][days] = max(profit[k][days-1]  , maxProfit + A[days]);
+            }
+        }
+
+        int ans = profit[K][N-1];
+        for(int i = 0 ; i<=K ; i++){
+            delete [] profit[i] ;
+        }
+        delete [] profit;
+        return ans;
     }
 
     //compplete 
